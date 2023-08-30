87e0618b43279ddc216c7d9498fb1ab3166d981a
Wsanbey <61056410+Wsanbey@users.noreply.github.com>
2023-08-30 17:21:15 -0300

    Projeto pronto
    Versionei o projeto localmente e agora finalizado momentaniamente.

---
+++ ContaBanco/src/ContaTerminal.java
@@ -0,0 +1,31 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class ContaTerminal {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Vamos criar uma conta bancaria?\n");
+        System.out.print("Por favor, digite o número da Agência: ");
+        String agencia = scanner.nextLine();
+
+        System.out.print("Por favor, digite o número da Conta: ");
+        int numero = scanner.nextInt();
+        scanner.nextLine(); // Limpar o buffer
+
+        System.out.print("Por favor, digite o nome do Cliente: ");
+        String nomeCliente = scanner.nextLine();
+
+        System.out.print("Por favor, digite o saldo: ");
+        double saldo = scanner.nextDouble();
+
+        scanner.close();
+
+        String mensagem = "Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " +
+                agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque.";
+
+        System.out.println(mensagem);
+    }
+}
